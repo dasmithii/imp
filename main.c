0451f72d08c23bd21fc49300a9f416599741681d
Adam Smith <iihtimsad@gmail.com>
2015-04-28 20:52:49 -0400

    setup file reading

--- main.c
+++ main.c
@@ -1,18 +1,48 @@
 #include "stdio.h"
 #include "parser.h"
+#include "string.h"
+#include "stdlib.h"
+
+
+static const char *const usage = 
+  "\tusage:"
+"\n\t  imp <file> ---------------- Interpret file."
+"\n\t  imp help ------------------ Display this message.\0";
+
+
+char *readFile(char *path){
+	FILE *stream;
+	char *contents;
+	size_t fileSize = 0;
+	stream = fopen(path, "rb");
+	if(!stream){
+		return NULL;
+	}
+	fseek(stream, 0L, SEEK_END);
+	fileSize = ftell(stream);
+	fseek(stream, 0L, SEEK_SET);
+	contents = malloc(fileSize+1);
+	size_t size=fread(contents,1,fileSize,stream);
+	contents[size]=0; 
+	fclose(stream);
+	return contents;
+}
+
+
 
 int main(int argc, char **argv){
-	ParseTree tree;
-	ParseTree_init(&tree, "(print -'tom') \n(add 5 -5 'adam')");
-	ParseTree_print(&tree);
-	// switch(argc){
-	// case 1:
-	// 	printf("%s\n", "repl");
-	// 	break;
-	// case 2:
-	// 	printf("%s\n", argv[1]);
-	// 	break;
-	// default:
-	// 	printf("%s\n", "invalid arguments");
-	// }
+	if(argc == 2){
+		if(strcmp(argv[1], "help") == 0){
+			printf("%s", usage);
+		} else{
+			char *code = readFile(argv[1]);
+			if(!code){
+				printf("Failed to read file.\n");
+				return -1;
+			}
+			printf("%s\n", code);
+		}
+	} else{
+		printf("%s", usage);
+	}
 }
