c4970a6d4c3d4db7e080a9748ad52eca3fdc6ec1
Adam Smith <iihtimsad@gmail.com>
2015-05-27 04:37:05 -0400

    removed unused files

--- root.h
+++
@@ -1,9 +0,0 @@
-// #ifndef IMP_ROOT_H_
-// #define IMP_ROOT_H_
-// #include <stdbool.h>
-
-// void *SoCache_load(char *file);
-// void SoCache_remove(char *file);
-// void SoCache_update(char *file);
-
-// #endif
