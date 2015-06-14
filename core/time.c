85f31fda59a820775a98b1542dc5f6618654f404
Adam Smith <iihtimsad@gmail.com>
2015-06-14 18:28:26 -0400

    started on core/time module

---
+++ core/time.c
@@ -0,0 +1,78 @@
+#include <imp/runtime.h>
+#include <imp/object.h>
+#include <imp/builtin/number.h>
+#include <imp/builtin/general.h>
+
+
+#define MICROSECONDS_PER_SECOND 1000000
+#define MICROSECONDS_PER_MILLISECOND 1000
+#define SECONDS_PER_MINUTE 60
+#define MINUTE_PER_HOUR 60
+
+
+Object *time_seconds(Runtime *runtime
+	               , Object *context
+	               , Object *module
+	               , int argc
+	               , Object **argv){
+	if(argc != 1){
+		Runtime_throwString(runtime, "time:seconds requires exactly one argument");
+	}
+	if(BuiltIn_id(argv[0]) != BUILTIN_NUMBER){
+		Runtime_throwString(runtime, "time:seconds requires numeric argument");
+	}
+	Object *r = Runtime_cloneField(runtime, "Number");
+	ImpNumber_setRaw(r, MICROSECONDS_PER_SECOND * ImpNumber_getRaw(argv[0]));
+	return r;
+}
+
+
+Object *time_milliseconds(Runtime *runtime
+	                    , Object *context
+	                    , Object *module
+	                    , int argc
+	                    , Object **argv){
+	if(argc != 1){
+		Runtime_throwString(runtime, "time:milliseconds requires exactly one argument");
+	}
+	if(BuiltIn_id(argv[0]) != BUILTIN_NUMBER){
+		Runtime_throwString(runtime, "time:milliseconds requires numeric argument");
+	}
+	Object *r = Runtime_cloneField(runtime, "Number");
+	ImpNumber_setRaw(r, MICROSECONDS_PER_MILLISECOND * ImpNumber_getRaw(argv[0]));
+	return r;
+}
+
+
+Object *time_minutes(Runtime *runtime
+	               , Object *context
+	               , Object *module
+	               , int argc
+	               , Object **argv){
+	if(argc != 1){
+		Runtime_throwString(runtime, "time:minutes requires exactly one argument");
+	}
+	if(BuiltIn_id(argv[0]) != BUILTIN_NUMBER){
+		Runtime_throwString(runtime, "time:minutes requires numeric argument");
+	}
+	Object *r = Runtime_cloneField(runtime, "Number");
+	ImpNumber_setRaw(r, SECONDS_PER_MINUTE * MICROSECONDS_PER_SECOND * ImpNumber_getRaw(argv[0]));
+	return r;
+}
+
+
+Object *time_hours(Runtime *runtime
+	             , Object *context
+	             , Object *module
+	             , int argc
+	             , Object **argv){
+	if(argc != 1){
+		Runtime_throwString(runtime, "time:hours requires exactly one argument");
+	}
+	if(BuiltIn_id(argv[0]) != BUILTIN_NUMBER){
+		Runtime_throwString(runtime, "time:hours requires numeric argument");
+	}
+	Object *r = Runtime_cloneField(runtime, "Number");
+	ImpNumber_setRaw(r, MINUTE_PER_HOUR * SECONDS_PER_MINUTE * MICROSECONDS_PER_SECOND * ImpNumber_getRaw(argv[0]));
+	return r;
+}
